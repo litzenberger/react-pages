e2c41306cc053a5ce708d85824d6790c6a36619e
ron litzenberger <ron.litzenberger@nearform>
2019-08-02 14:15:27 -0500

    Updates

---
+++ precache-manifest.fa908f2e907f6505e700774ec8e501fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8373a12fe6fe4b1e1e8dc6ba4748559",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "0288d22504b1bdf9e6b7",
+    "url": "/react-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0288d22504b1bdf9e6b7",
+    "url": "/react-pages/static/js/main.959da0ce.chunk.js"
+  },
+  {
+    "revision": "1e067ef579591dacb6c4",
+    "url": "/react-pages/static/js/runtime~main.2afdb07a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
